3ec8b7759df98a4a9276b69def27632c45992531
Zhabka18 <116446977+Zhabka18@users.noreply.github.com>
2025-11-09 19:51:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
+// Авторизуем пользователя, используя инфу о нем.
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
+// Делаем запрос за инфой о пользователе.
+const fetchYandexData = (token) =>
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+    (res) => res.json()
+  );
+
 window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "54c6922787f5412588f8ca822446dd33",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://oauth-master-class3.vercel.app/token.html",
       },
-      "https://examplesite.com",
-      {
-        view: "button",
-        parentId: "buttonContainer",
-        buttonSize: "m",
-        buttonView: "main",
-        buttonTheme: "light",
-        buttonBorderRadius: "0",
-        buttonIcon: "ya",
-      }
+      "https://oauth-master-class3.vercel.app",
     )
       .then(({ handler }) => handler())
-      .then((data) => console.log("Сообщение с токеном", data))
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+        authorize(result);
+        console.log(result, data);
+      })
       .catch((error) => console.log("Обработка ошибки", error));
   };
 };
